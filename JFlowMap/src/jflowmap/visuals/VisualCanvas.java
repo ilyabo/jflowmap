b77b55dea81fbc98d2c3c6fdbfcf58d1e08e2689
ilyabo <ilyabo@82f1accc-f168-11de-82c3-17f7e48890e1>
2010-05-31 13:12:35 +0000

    canvas autofit when bounds changed

--- JFlowMap/src/jflowmap/visuals/VisualCanvas.java
+++ JFlowMap/src/jflowmap/visuals/VisualCanvas.java
@@ -18,6 +18,12 @@
 
 package jflowmap.visuals;
 
+import java.awt.Insets;
+import java.awt.geom.Rectangle2D;
+
+import jflowmap.util.piccolo.PanHandler;
+import jflowmap.util.piccolo.PiccoloUtils;
+import jflowmap.util.piccolo.ZoomHandler;
 import edu.umd.cs.piccolo.PCamera;
 import edu.umd.cs.piccolo.PCanvas;
 import edu.umd.cs.piccolo.util.PBounds;
@@ -27,6 +33,12 @@ import edu.umd.cs.piccolo.util.PBounds;
  */
 public class VisualCanvas extends PCanvas {
 
+  public VisualCanvas() {
+    setZoomEventHandler(null);
+    addInputEventListener(new ZoomHandler());
+    setPanEventHandler(new PanHandler());
+  }
+
   public void fitChildrenInCameraView() {
     PCamera camera = getCamera();
     PBounds boundRect = getLayer().getFullBounds();
@@ -34,4 +46,76 @@ public class VisualCanvas extends PCanvas {
     camera.animateViewToCenterBounds(boundRect, true, 0);
   }
 
+
+  @Override
+  public void setBounds(int x, int y, int w, int h) {
+    Rectangle2D oldVisibleBounds =  getVisibleBounds();
+    Rectangle2D oldContentBounds = getViewContentBounds();
+
+    super.setBounds(x, y, w, h);
+
+    if (oldVisibleBounds != null) {
+      fitVisibleInCameraView(oldVisibleBounds, oldContentBounds);
+    }
+  }
+
+  private static final Insets NULL_INSETS = new Insets(0, 0, 0, 0);
+
+  public void fitVisibleInCameraView(Rectangle2D visibleBounds, Rectangle2D contentBounds) {
+    if (contentBounds.getWidth() <= 0 || contentBounds.getHeight() <= 0) {
+      return;
+    }
+    final Insets insets;
+//    if (contentBounds.contains(visibleBounds)) { // the whole  is inside the view
+//      insets = getContentInsets();
+//      insets.left += 5;
+//      insets.top += 5;
+//      insets.bottom += 5;
+//      insets.right += 5;
+//    } else {
+      insets = NULL_INSETS;
+//    }
+    PiccoloUtils.setViewPaddedBounds(getCamera(), visibleBounds, insets);
+  }
+
+
+  /**
+   * Bounds of the part of the visualization (all of the Nodes together) that
+   * is currently visible in the view (in the view coordinate system).
+   *
+   * @return
+   */
+  public Rectangle2D getVisibleBounds() {
+//      final PBounds mb = getBounds();
+      final PBounds mb = getLayer().getFullBounds();
+      if (mb != null) {
+          final PBounds vb = getCamera().getViewBounds();
+          Rectangle2D.intersect(vb, mb, vb);
+          return vb;
+      }
+      return null;
+  }
+
+  /**
+   * Bounds of the area where the visualization should be placed (without the floating
+   * panels) in the camera coordinate system.
+   *
+   * @return
+   */
+  private Rectangle2D getContentBounds() {
+//    final Insets insets = getContentInsets();
+//    final PBounds vb = getCamera().getBounds();
+//    vb.x += insets.left;
+//    vb.y += insets.top;
+//    vb.width -= insets.left + insets.right;
+//    vb.height -= insets.top + insets.bottom;
+//    return vb;
+    return getBounds();
+  }
+
+  private Rectangle2D getViewContentBounds() {
+    final Rectangle2D cb = getContentBounds();
+    getCamera().localToView(cb);
+    return cb;
+  }
 }
